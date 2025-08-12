ae04e447804d300d55e2676ba4ca9e9920bd387a
Kanrawee2334 <kanrawee214@gmail.com>
2025-08-12 12:55:08 +0700

    Update app.py

--- app.py
+++ app.py
@@ -187,4 +187,4 @@ async def history7():
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
